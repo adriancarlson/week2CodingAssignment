368ec450c58d4b49caeed2912671fb6ccea61649
adriancarlson <adrian.r.carlson@gmail.com>
2021-03-30 21:15:15 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// 1.	What do each of the following Boolean expressions evaluate to?
+// Boolean Expression	Answer
+// true && false	
+// true || false	
+// false && false	
+// true && (false || true)	
+// false || (true && false)	
+// false || 1 < 5	
+// 5 >= 4 && 1 > 3	
+// 10 < 4 || 1 > 4	
+// 12 >= 2 && 1 < 24	
+// ‘Hello’.charAt(0) == ‘h’	
